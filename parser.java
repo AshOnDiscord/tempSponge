ffba5d5817471dd2941150d8b748dec8e63da40b
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2023-08-20 18:50:09 -0400

    remove useless import

--- parser.java
+++ parser.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.DecimalFormat;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
